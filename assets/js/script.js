ee48d710798e7bb0d1f79086b4742eede933e290
Joshua Gottlieb <jpikachu1999@gmail.com>
2022-04-25 15:29:31 -0400

    initial commit, flip button implemented and working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,45 @@
 // TODO: Declare any global variables we need
 
-
-document.addEventListener('DOMContentLoaded', function () {
-    // This is just a sanity check to make sure your JavaScript script is getting loaded
-    // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
+let pennyState = document.getElementById("penny-image");
+let flipButton = document.getElementById("flip");
+let clearButton = document.getElementById("clear");
+let headsAbsCount = document.getElementById("heads");
+let headsPerCount = document.getElementById("heads-percent");
+let tailsAbsCount = document.getElementById("tails");
+let tailsPerCount = document.getElementById("tails-percent");
+let headsAbs = 0;
+let headsPer = 0;
+let tailsAbs = 0;
+let tailsPer = 0;
+let flipNum = 0;
 
     // TODO: Add event listener and handler for flip and clear buttons
+flipButton.addEventListener('click', function() {
+    flipNum++;
+    let flipResult = Math.floor(Math.random() * 2);
+
+    if(flipResult === 0) {
+        pennyState.src = "./assets/images/penny-heads.jpg";
+        headsAbs++;
+        headsPer = +((headsAbs / flipNum) * 100).toFixed(2);
+        tailsPer = +((tailsAbs / flipNum) * 100).toFixed(2);
+        
+        headsAbsCount.textContent = headsAbs;
+        headsPerCount.textContent = `${headsPer}\%`;
+        tailsPerCount.textContent = `${tailsPer}\%`;
+    }
+    else{
+        pennyState.src = "./assets/images/penny-tails.jpg";
+        tailsAbs++;
+        headsPer = +((headsAbs / flipNum) * 100).toFixed(2);
+        tailsPer = +((tailsAbs / flipNum) * 100).toFixed(2);
+        
+        tailsAbsCount.textContent = tailsAbs;
+        headsPerCount.textContent = `${headsPer}\%`;
+        tailsPerCount.textContent = `${tailsPer}\%`;
+    }
+})
+
 
     // Flip Button Click Handler
         // TODO: Determine flip outcome
@@ -23,5 +56,3 @@ document.addEventListener('DOMContentLoaded', function () {
     // Clear Button Click Handler
         // TODO: Reset global variables to 0
         // TODO: Update the scoreboard (same logic as in flip button click handler)
-
-})
